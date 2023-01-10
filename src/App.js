7e1155c1d057f8b5d5afee52a8141244cb51ebb7
HoangDat0412 <hoang2811dat@gmail.com>
2023-01-10 21:24:11 +0700

    tạo layout xây dựng router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Projects from './Pages/Projects';
+import DefaultLayout from './layout/DefaultLayout';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+     <Router>
+        <div className="App">
+          
+          <Routes>
+            <Route path='/' element={<DefaultLayout><Home></Home></DefaultLayout>}></Route>
+            <Route path='/projects' element={<DefaultLayout><Projects></Projects></DefaultLayout>}></Route>
+            <Route path='/about' element={<DefaultLayout><About></About></DefaultLayout>}></Route>
+          </Routes>
+      
+        </div>
+     </Router>
+  
   );
 }
 
