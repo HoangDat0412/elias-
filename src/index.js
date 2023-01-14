b351c664db38b9282e2ce514b3486f2007cc4192
HoangDat0412 <hoang2811dat@gmail.com>
2023-01-14 16:13:53 +0700

    Hoàn thiện elias

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import GlobalStyle from './Components/GlobalStyles';
 
+
+import $ from 'jquery';  
+import Popper from 'popper.js';  
+import 'bootstrap/dist/js/bootstrap.bundle.min';  
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  
--- src/layout/DefaultLayout/index.js
+++ src/layout/DefaultLayout/index.js
@@ -1,12 +1,19 @@
 import Header from "./Header";
+import './DefaultLayout.scss'
 
+import Footer from "./Footer";
+import  className  from "classnames";
 function DefaultLayout({ children}) {
     return ( 
-        <div>
+        <div >
             <Header></Header>
-            <div className="content">
+
+            <div className={ className('content','container-xl',"container-fluid")}>
                {children}
             </div>
+
+            <Footer></Footer>
+
         </div>
      );
 }
